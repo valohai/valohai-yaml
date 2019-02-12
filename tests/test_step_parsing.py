89028c3f5fade6f6207948c0c8413c8474afd65e
Juha Kiili <juha@juhakiili.com>
2019-02-12 15:14:44 +0200

    Fix optional flag parsing (fixes #935)

--- tests/test_step_parsing.py
+++ tests/test_step_parsing.py
@@ -47,8 +47,13 @@ def test_boolean_param_parse(boolean_param_config):
     step = boolean_param_config.steps['test']
     assert step.parameters['case-insensitive'].optional
     assert step.parameters['case-insensitive'].choices == (True, False)
+    assert step.parameters['defaults-to-true'].optional
+    assert step.parameters['defaults-to-true'].choices == (True, False)
     assert step.build_command({'case-insensitive': True}) == ['foo --case-insensitive']
     assert step.build_command({'case-insensitive': False}) == ['foo']
+    assert step.build_command({'defaults-to-true': True}) == ['foo --defaults-to-true']
+    assert step.build_command({'defaults-to-true': False}) == ['foo']
+    assert step.build_command({}) == ['foo']
 
 
 def test_mount_parse(mount_config):
