26bfaa36c50575c9ab42021aca58a1d48cd89b76
Aarni Koskela <akx@iki.fi>
2019-08-01 15:20:22 +0300

    Modernize the pipeline example slightly

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -2,10 +2,18 @@ from valohai_yaml.objs import Config
 
 
 def test_pipeline(pipeline_config: Config):
-    e0 = pipeline_config.pipelines['My little pipeline'].edges[0]
-    assert e0.source_node == 'batch1'
-    assert e0.source_type == 'parameter'
-    assert e0.source_key == 'aspect-ratio'
-    assert e0.target_node == 'batch2'
-    assert e0.target_type == 'parameter'
-    assert e0.target_key == 'aspect-ratio'
+    lr = pipeline_config.lint()
+    assert lr.is_valid()
+    assert len([edge for edge in pipeline_config.pipelines['My little pipeline'].edges if (
+        edge.source_node == 'batch1' and
+        edge.source_type == 'parameter' and
+        edge.source_key == 'aspect-ratio' and
+        edge.target_node == 'batch2' and
+        edge.target_type == 'parameter' and
+        edge.target_key == 'aspect-ratio'
+    )]) == 1
+    assert any(
+        (edge.source_type == 'output' and edge.source_key == 'model.pb')
+            for edge
+            in pipeline_config.pipelines['My medium pipeline'].edges
+    )
