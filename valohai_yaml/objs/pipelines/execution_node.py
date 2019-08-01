9c058304c9ad1ad8c9220bc9f098a9dcf80700b9
Aarni Koskela <akx@iki.fi>
2019-08-01 15:19:57 +0300

    Add linting for pipeline step existence

--- valohai_yaml/objs/pipelines/execution_node.py
+++ valohai_yaml/objs/pipelines/execution_node.py
@@ -10,3 +10,14 @@ class ExecutionNode(Node):
         self.name = name
         self.step = step
         self.override = override
+
+    def lint(self, lint_result, context):
+        super().lint(lint_result, context)
+        config = context['config']
+        pipeline = context['pipeline']
+        if self.step not in config.steps:
+            lint_result.add_error('Pipeline {pipeline} node {node} step {step} does not exist'.format(
+                pipeline=pipeline.name,
+                node=self.name,
+                step=self.step,
+            ))
