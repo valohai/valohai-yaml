e28664be0619de1c69024ef31c256a1a0c98173c
Aarni Koskela <akx@iki.fi>
2020-10-15 13:30:19 +0300

    Add --strict-warnings mode to CLI

--- valohai_yaml/__main__.py
+++ valohai_yaml/__main__.py
@@ -2,31 +2,43 @@ import argparse
 import sys
 from typing import List, Optional
 
-from valohai_yaml.lint import lint
+from valohai_yaml.lint import lint, LintResult
 
 
 def main(args: Optional[List[str]] = None) -> int:
     ap = argparse.ArgumentParser()
+    ap.add_argument(
+        '--strict-warnings',
+        action='store_true',
+        help='exit with error if there are warnings',
+    )
     ap.add_argument('file', nargs='+', help='file(s) to validate')
     args = ap.parse_args(args)
-    errors = 0
+    errors = warnings = 0
     for file in args.file:
-        errors += process_file(file)
-    return (1 if errors > 0 else 0)
+        result = process_file(file)
+        errors += result.error_count
+        warnings += result.warning_count
+
+    if errors or warnings:
+        print('*** {} errors, {} warnings'.format(errors, warnings))
+
+    return 1 if errors or (args.strict_warnings and warnings) else 0
 
 
-def process_file(file: str) -> int:
+def process_file(file: str) -> LintResult:
     header_printed = False
 
     with open(file, 'rb') as stream:
         result = lint(stream)
-        for item in result.messages:
-            if not header_printed:
-                print('>>>', file)
-                header_printed = True
-            print(item['message'])
-            print('-' * 60)
-        return result.error_count
+
+    for item in result.messages:
+        if not header_printed:
+            print('>>>', file)
+            header_printed = True
+        print('{}: {}'.format(item['type'], item['message']))
+        print('-' * 60)
+    return result
 
 
 if __name__ == "__main__":
