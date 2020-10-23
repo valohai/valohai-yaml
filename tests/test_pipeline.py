0ba68f9b9706441432e659833fc79807e43ff424
magda <stenius.mimmy@gmail.com>
2020-10-23 13:21:45 +0300

    Add deployment version alias list

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -15,17 +15,28 @@ def test_pipeline(pipeline_config: Config):
         )
         for edge in pipeline_config.pipelines["My little pipeline"].edges
     )
+
     assert any(
         (
             edge.source_node == "train"
             and edge.source_type == "output"
             and edge.source_key == "model"
-            and edge.target_node == "deploy"
+            and edge.target_node == "deploy-predictor"
             and edge.target_type == "file"
             and edge.target_key == "predict-digit.model"
         )
         for edge in pipeline_config.pipelines["My deployment pipeline"].edges
     )
+    dp = pipeline_config.pipelines["My deployment pipeline"]
+
+    dn_predict = [node for node in dp.nodes if node.type == 'deployment' and node.name == 'deploy-predictor'][0]
+    assert "predictor-staging" in dn_predict.aliases
+    assert "predict-digit" in dn_predict.endpoints
+
+    dn_no_preset = [node for node in dp.nodes if node.type == 'deployment' and node.name == 'deploy-no-presets'][0]
+    assert dn_no_preset.aliases == []
+    assert dn_no_preset.endpoints == []
+
     assert any(
         (edge.source_type == "output" and edge.source_key == "model.pb")
         for edge in pipeline_config.pipelines["My medium pipeline"].edges
