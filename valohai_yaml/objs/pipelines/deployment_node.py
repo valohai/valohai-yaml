916a1ab340abdf20bfe32115f43d4866c6d3cd67
magda <stenius.mimmy@gmail.com>
2020-10-23 13:21:45 +0300

    Add deployment version alias list

--- valohai_yaml/objs/pipelines/deployment_node.py
+++ valohai_yaml/objs/pipelines/deployment_node.py
@@ -1,11 +1,4 @@
 from .node import Node
-from ..base import Item
-
-
-class Deployment(Item):
-    def __init__(self, name, endpoints) -> None:
-        self.name = name
-        self.endpoints = endpoints
 
 
 class DeploymentNode(Node):
@@ -16,11 +9,13 @@ class DeploymentNode(Node):
         *,
         name,
         deployment,
-        endpoints
+        endpoints=[],
+        aliases=[]
     ) -> None:
         self.name = name
         self.deployment = deployment
         self.endpoints = endpoints
+        self.aliases = aliases
 
     def lint(self, lint_result, context: dict) -> None:
         super().lint(lint_result, context)
@@ -29,6 +24,3 @@ class DeploymentNode(Node):
             lint_result.add_error(
                 'Deployment has no name'
             )
-
-        if not self.endpoints:
-            lint_result.add_error('Deployment {} has no endpoints'.format(self.name))
