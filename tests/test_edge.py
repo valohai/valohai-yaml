121cb241df800d40eb60fcac4f58f1feaa16d775
Aarni Koskela <akx@iki.fi>
2021-07-29 09:46:00 +0300

    Raise a ValidationError when an edge specifier (a.b.c) has the wrong number of items

---
+++ tests/test_edge.py
@@ -0,0 +1,12 @@
+import pytest
+
+from valohai_yaml.excs import ValidationError
+from valohai_yaml.objs import Edge
+
+
+def test_edge_dots():
+    with pytest.raises(ValidationError):
+        Edge(source='foo.bar', target='baz.quux')
+    e = Edge(source='foo.bar.baz', target='baz.quux.peasoup')
+    with pytest.raises(ValidationError):
+        e.source = '8'
