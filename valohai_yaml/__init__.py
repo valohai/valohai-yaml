f4d21db6a9db018894875f8ac37f8bc3aa4db690
Aarni Koskela <akx@iki.fi>
2021-09-28 09:30:32 +0300

    Do re-exports in __init__ modules

--- valohai_yaml/__init__.py
+++ valohai_yaml/__init__.py
@@ -1,5 +1,12 @@
-from .excs import ValidationErrors  # noqa
-from .parsing import parse  # noqa
-from .validation import validate  # noqa
+from .excs import ValidationErrors
+from .parsing import parse
+from .validation import validate
 
 __version__ = '0.15.0'
+
+__all__ = [
+    'ValidationErrors',
+    '__version__',
+    'parse',
+    'validate',
+]
