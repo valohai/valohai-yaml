2ad5f32fc025d619c1e25f6a649fac1ab6da2bb3
ruksi <me@ruk.si>
2021-10-05 11:47:00 +0300

    Add optional `resources` to endpoint definition

--- tests/test_endpoint_parsing.py
+++ tests/test_endpoint_parsing.py
@@ -1,14 +1,25 @@
-def test_endpoint_parse(endpoint_config):
-    server_endpoint = endpoint_config.endpoints['server-endpoint']
-    assert server_endpoint.image == 'python:3.6'
-    assert server_endpoint.port == 1453
-    assert server_endpoint.server_command == 'python run_server.py'
-    wsgi_endpoint = endpoint_config.endpoints['wsgi-endpoint']
-    assert wsgi_endpoint.description == 'predict digits from image inputs'
-    assert wsgi_endpoint.image == 'tensorflow/tensorflow:1.3.0-py3'
-    assert wsgi_endpoint.wsgi == 'predict_wsgi:predict_wsgi'
-    assert len(wsgi_endpoint.files) == 1
-    file = wsgi_endpoint.files[0]
+def test_server_endpoint_parse(endpoint_config):
+    endpoint = endpoint_config.endpoints['server-endpoint']
+    assert endpoint.image == 'python:3.6'
+    assert endpoint.port == 1453
+    assert endpoint.server_command == 'python run_server.py'
+
+
+def test_wsgi_endpoint_parse(endpoint_config):
+    endpoint = endpoint_config.endpoints['wsgi-endpoint']
+    assert endpoint.description == 'predict digits from image inputs'
+    assert endpoint.image == 'tensorflow/tensorflow:1.3.0-py3'
+    assert endpoint.wsgi == 'predict_wsgi:predict_wsgi'
+    assert len(endpoint.files) == 1
+    file = endpoint.files[0]
     assert file.name == 'model'
     assert file.description == 'Model output file from TensorFlow'
     assert file.path == 'model.pb'
+
+
+def test_limited_endpoint_parse(endpoint_config):
+    endpoint = endpoint_config.endpoints['limited-endpoint']
+    assert endpoint.resources['cpu']['min'] == 0.1
+    assert endpoint.resources['cpu']['max'] == 1.0
+    assert endpoint.resources['memory']['min'] == 50
+    assert endpoint.resources['memory']['max'] == 100
