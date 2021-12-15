e0a337695029f8f064748c8b7ecdafe0e8a5bcde
Aarni Koskela <akx@iki.fi>
2021-12-15 09:42:24 +0200

    Become 0.21.0

--- valohai_yaml/__init__.py
+++ valohai_yaml/__init__.py
@@ -2,7 +2,7 @@ from .excs import ValidationErrors
 from .parsing import parse
 from .validation import validate
 
-__version__ = '0.20.1'
+__version__ = '0.21.0'
 
 __all__ = [
     'ValidationErrors',
