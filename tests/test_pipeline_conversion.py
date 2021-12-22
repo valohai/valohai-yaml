265927a825d5bda319704d2a5209e4d1765e76d8
Aarni Koskela <akx@iki.fi>
2021-12-22 14:31:12 +0200

    Correct order of applying overrides to step data when converting nodes

--- tests/test_pipeline_conversion.py
+++ tests/test_pipeline_conversion.py
@@ -10,3 +10,13 @@ def test_pipeline_conversion_smoke(pipeline_config: Config):
         ).convert_pipeline(pipeline)
         assert result["nodes"]
         assert result["edges"]
+
+
+def test_pipeline_conversion_override_order(pipeline_config: Config):
+    result = PipelineConverter(
+        config=pipeline_config,
+        commit_identifier="latest",
+    ).convert_pipeline(pipeline_config.pipelines["My medium pipeline"])
+    train_node = next(node for node in result["nodes"] if node["name"] == "train")
+    # If conversion order is incorrect, this will have remained a list
+    assert isinstance(train_node["template"]["inputs"], dict)
