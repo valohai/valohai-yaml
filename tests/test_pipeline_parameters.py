8cc593df39078eb2db674446dfdf20c174661fca
Aarni Koskela <akx@iki.fi>
2022-09-08 13:13:06 +0300

    Similarly to node actions, don't serialize empty pipeline parameters

--- tests/test_pipeline_parameters.py
+++ tests/test_pipeline_parameters.py
@@ -5,3 +5,5 @@ def test_programmatic_pipeline_parameters():
     parameterless_pipeline = Pipeline(name="...", nodes=[], edges=[])
     # Test that parameters aren't required
     assert parameterless_pipeline.parameters == []
+    # Test that empty parameters are not serialized
+    assert "parameters" not in parameterless_pipeline.serialize()
