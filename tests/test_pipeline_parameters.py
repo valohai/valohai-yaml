9555ccd5f891df958d5a8b20a8847c066e897818
Aarni Koskela <akx@iki.fi>
2022-09-08 13:17:53 +0300

    Add tests for pipeline parameters

--- tests/test_pipeline_parameters.py
+++ tests/test_pipeline_parameters.py
@@ -1,4 +1,6 @@
-from valohai_yaml.objs import Pipeline
+import pytest
+
+from valohai_yaml.objs import Pipeline, PipelineParameter
 
 
 def test_programmatic_pipeline_parameters():
@@ -7,3 +9,25 @@ def test_programmatic_pipeline_parameters():
     assert parameterless_pipeline.parameters == []
     # Test that empty parameters are not serialized
     assert "parameters" not in parameterless_pipeline.serialize()
+
+
+def test_pipeline_parameters(pipeline_with_parameters_config):
+    pipe = pipeline_with_parameters_config.pipelines['Example Pipeline with Parameters']
+    assert len(pipe.parameters) == 1
+    param = pipe.parameters[0]
+    assert isinstance(param, PipelineParameter)
+    assert param.name == 'id'
+    assert param.targets == [
+        'train.parameter.id',
+        'train_parallel.parameter.id',
+    ]
+
+
+@pytest.mark.parametrize('name', ('Short hand parameter target', 'Short hand parameter target 2'))
+def test_pipeline_parameter_shorthand(pipeline_with_parameters_config, name):
+    pipe = pipeline_with_parameters_config.pipelines[name]
+    assert len(pipe.parameters) == 1
+    param = pipe.parameters[0]
+    assert isinstance(param, PipelineParameter)
+    assert param.name == 'id'
+    assert param.targets == ['train.parameter.id']
