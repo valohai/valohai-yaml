68f803706981ab1619688f40180d13065ca52a3a
Aarni Koskela <akx@iki.fi>
2023-08-03 10:32:55 +0300

    Update valohai_yaml/pipelines/conversion.py

--- valohai_yaml/pipelines/conversion.py
+++ valohai_yaml/pipelines/conversion.py
@@ -62,10 +62,9 @@ class PipelineConverter:
         step_data = step.serialize()
         step_data.update(override)
 
-        runtime_config = step_data.get("runtime_config", {})
-        if 'no-output-timeout' in step_data:
-            runtime_config.setdefault('no_output_timeout', step_data.pop('no-output-timeout'))
-        step_data["runtime_config"] = runtime_config
+        runtime_config = step_data.setdefault("runtime_config", {})
+        if "no-output-timeout" in step_data:
+            runtime_config.setdefault("no_output_timeout", step_data.pop("no-output-timeout"))
 
         parameters_from_node = node.get_parameter_defaults()
         parameters_from_step = step.get_parameter_defaults(include_flags=True)
