fd29ea3d3d378f84e09bda635eb46a9002130c8b
Eugene Volen <132352175+euvolen-valohai@users.noreply.github.com>
2023-08-10 09:50:59 +0300

    Add tests

--- tests/test_pipeline_conversion.py
+++ tests/test_pipeline_conversion.py
@@ -30,3 +30,23 @@ def test_pipeline_conversion_no_output_timeout(pipeline_config: Config):
     ).convert_pipeline(pipeline_config.pipelines["My medium pipeline"])
     train_node = next(node for node in result["nodes"] if node["name"] == "train")
     assert train_node["template"]["runtime_config"]["no_output_timeout"] == parse_duration_string("6h").total_seconds()
+
+def test_pipeline_conversion_override_inputs(pipeline_overridden_config: Config):
+    result = PipelineConverter(
+        config=pipeline_overridden_config,
+        commit_identifier="latest",
+    ).convert_pipeline(pipeline_overridden_config.pipelines["My overriden input pipeline"])
+
+    merged = next(node for node in result["nodes"] if node["name"] == "merged")
+    assert merged["template"]["command"][0] == 'merge date'
+    assert merged["template"]["image"] == 'merge node image'
+    assert isinstance(merged["template"]["inputs"], dict)
+    assert len(merged["template"]["inputs"].get('training-images', [])) == 2
+    assert merged["template"]["inputs"].get('training-images', [])[0] == 'merged node image 1'
+    assert len(merged["template"]["parameters"].items()) == 3
+
+    overridden = next(node for node in result["nodes"] if node["name"] == "overridden")
+    assert isinstance(overridden["template"]["inputs"], dict)
+    assert overridden["template"]["inputs"].get('training-images', [])[0] == 'overridden node image'
+    assert len(overridden["template"]["inputs"].get('training-images', [])) == 1
+    assert len(overridden["template"]["parameters"].items()) == 3
