d343bca91abcacba32ed9a5e0c978fbacfeed913
Aarni Koskela <akx@iki.fi>
2023-10-30 13:26:08 +0200

    Rename step_with_tasks fixture; DRY test (#130)

--- tests/test_task.py
+++ tests/test_task.py
@@ -2,14 +2,12 @@ from valohai_yaml.objs import Config, Task
 from valohai_yaml.objs.variant_parameter import VariantParameterStyle
 
 
-def test_tasks_parameters(step_with_tasks: Config):
-    steps = step_with_tasks.steps
-    tasks = step_with_tasks.tasks
-    for task in tasks:
-        assert isinstance(tasks[task], Task)
-        assert tasks[task].name == task
-        assert tasks[task].step in steps
-        assert isinstance(tasks[task].parameters, list)
-        if len(tasks[task].parameters) > 0:
-            assert tasks[task].parameters[0].name == "A"
-            assert isinstance(tasks[task].parameters[0].style, VariantParameterStyle)
+def test_tasks_parameters(task_config: Config):
+    for name, task in task_config.tasks.items():
+        assert isinstance(task, Task)
+        assert task.name == name
+        assert task.step in task_config.steps
+        assert isinstance(task.parameters, list)
+        if len(task.parameters) > 0:
+            assert task.parameters[0].name == "A"
+            assert isinstance(task.parameters[0].style, VariantParameterStyle)
