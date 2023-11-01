fbd68d39f9df9bb657ba5ea439446ba236c01ca3
Aarni Koskela <akx@iki.fi>
2023-11-01 16:45:50 +0200

    Use snapshot tests for lint tests

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import re
 
 import pytest
 
@@ -8,17 +9,33 @@ from tests.consts import (
     examples_path,
     invalid_obj,
     valid_bytes,
+    valid_examples_path,
     valid_obj,
     warning_examples_path,
 )
-from tests.utils import get_error_example_path, get_valid_example_path
 from valohai_yaml import ValidationErrors, validate
 from valohai_yaml.__main__ import main
+
+
+def assert_validation_output(capsys, snapshot, yaml_path: str) -> bool:
+    out, err = capsys.readouterr()
+    # Replace the path with just the filename,
+    # so this is agnostic to where the tests are run from.
+    out = out.replace(yaml_path, os.path.basename(yaml_path))
+    # Normalize Python version differences in certain error messages.
+    out = re.sub(
+        r"unterminated string literal \(.+?\)",
+        "EOL while scanning string literal",
+        out,
+    )
+    assert out == snapshot
+    return True
 
 
 @pytest.mark.parametrize(
     "good_example_path",
     glob.glob(os.path.join(examples_path, "*.yaml")),
+    ids=os.path.basename,
 )
 def test_good_examples_cli(good_example_path):
     """Test that known-good files validate via the CLI."""
@@ -26,65 +43,35 @@ def test_good_examples_cli(good_example_path):
 
 
 @pytest.mark.parametrize(
-    "bad_example_path",
+    "yaml_path",
     glob.glob(os.path.join(error_examples_path, "*.yaml")),
+    ids=os.path.basename,
 )
-def test_bad_examples_cli(capsys, bad_example_path):
+def test_bad_examples_cli(capsys, yaml_path, snapshot):
     """Test that bad examples don't validate via the CLI."""
-    assert main([bad_example_path]) == 1
-    out, err = capsys.readouterr()
-    assert out
+    assert main([yaml_path]) == 1
+    assert assert_validation_output(capsys, snapshot, yaml_path)
 
 
 @pytest.mark.parametrize(
     "yaml_path",
     glob.glob(os.path.join(warning_examples_path, "*.yaml")),
+    ids=os.path.basename,
 )
-def test_warning_examples_cli(capsys, yaml_path):
+def test_warning_examples_cli(capsys, yaml_path, snapshot):
     """Test that warning examples don't validate via the CLI in strict mode."""
     assert main(["--strict-warnings", yaml_path]) == 1
-    out, err = capsys.readouterr()
-    assert out
+    assert assert_validation_output(capsys, snapshot, yaml_path)
 
 
-def test_invalid_file_missing_properties_cli(capsys):
-    assert main([get_error_example_path("step-missing-required-properties.yaml")]) == 1
-    out, err = capsys.readouterr()
-    assert "step-missing-required-properties.yaml" in out
-    assert "'command' is a required property" in out
-    assert "'name' is a required property" in out
-    assert "8 is not valid under any of the given schemas" in out
-
-
-def test_invalid_file_too_long_input_name_cli(capsys):
-    assert main([get_error_example_path("input-name-too-long.yaml")]) == 1
-    out, err = capsys.readouterr()
-    assert "input-name-too-long.yaml" in out
-    assert (
-        "'this-input-name-is-way-too-long-and-will-cause-the-validation-to-fail' is too long"
-        in out
-    )
-
-
-def test_invalid_on_error_value(capsys):
-    assert main([get_error_example_path("invalid-on-error-value.yaml")]) == 1
-    out, err = capsys.readouterr()
-    assert "invalid-on-error-value.yaml" in out
-    assert "is not valid under any of the given schemas" in out
-
-
-def test_valid_endpoint_name_cli(capsys):
-    assert main([get_valid_example_path("endpoint-names-valid.yaml")]) == 0
-
-
-def test_invalid_endpoint_name_cli(capsys):
-    assert main([get_error_example_path("endpoint-names-invalid.yaml")]) == 1
-    out, err = capsys.readouterr()
-    assert "endpoint-names-invalid.yaml" in out
-    assert "'wsgi_endpoint' does not match '^[a-z][a-z0-9-]+$'" in out
-    assert "'server@endpoint' does not match '^[a-z][a-z0-9-]+$'" in out
-    assert "'3-server-endpoint' does not match '^[a-z][a-z0-9-]+$'" in out
-    assert "'Server-endpoint' does not match '^[a-z][a-z0-9-]+$'" in out
+@pytest.mark.parametrize(
+    "valid_example_path",
+    glob.glob(os.path.join(valid_examples_path, "*.yaml")),
+    ids=os.path.basename,
+)
+def test_valid_examples_cli(valid_example_path):
+    """Test that valid example files validate via the CLI."""
+    assert main([valid_example_path]) == 0
 
 
 def test_bytes_validation():
