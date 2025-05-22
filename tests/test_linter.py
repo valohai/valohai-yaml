e3e9b685e58fc8b92bafc14555b0347574e378af
Ruksi Korpisara <me@ruk.si>
2025-05-22 11:01:42 +0300

    Move bad optional flag example to under tests
    don't promote it under "examples" as the filename or contents don't
    communicate that it is a warning example

--- tests/test_linter.py
+++ tests/test_linter.py
@@ -10,13 +10,6 @@ from tests.utils import (
 from valohai_yaml.lint import lint_file
 
 
-# Parameters of 'flag' type do not logically support 'optional' property so we warn about it.
-def test_optional_flag():
-    items = lint_file("./examples/flag-optional-example.yaml")
-    warning = "Step test, parameter case-insensitive: `optional` has no effect on flag-type parameters"
-    assert any((warning in item["message"]) for item in items.warnings)  # pragma: no branch
-
-
 @pytest.mark.parametrize(
     "file, expected_message",
     [
